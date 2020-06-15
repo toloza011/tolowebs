49df126976e317bac41fae286d5bab80a8004d01
Esteban Toloza <estebantoloza1998@gmail.com>
2020-06-15 01:40:22 -0400

    Nuevo

--- config/database.php
+++ config/database.php
@@ -45,11 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'phpmyadmin'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'tolowebs'),
-            'username' => env('DB_USERNAME', 'toloza01'),
-            'password' => env('DB_PASSWORD', 'Tayson1998'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
