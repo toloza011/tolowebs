1f2e5841cfd14d8c536f19ffd74ec3348488e3d6
toloza01 <estebantoloza1998@gmail.com>
2020-09-14 14:05:08 -0300

    ruta index

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 //----------------Home-------------//
 
-Route::get('/','HomeController@home')->name('home');
+Route::get('/home','HomeController@home')->name('home');
 Route::get('/contacto','HomeController@contacto')->name('contacto');
 Route::post('/enviarContacto','HomeController@enviarContacto')->name('correo');
 
