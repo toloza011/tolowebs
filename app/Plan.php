0ffea1def59fd587ddd841c1971c3a48feb01c0e
toloza01 <estebantoloza1998@gmail.com>
2020-09-19 16:12:41 -0300

    deploy tolowebs

--- app/Plan.php
+++ app/Plan.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model
 {
     protected $table= "planes";
+    protected $primaryKey="id";
+    protected $fillable=['id','nombre','descripcion','precio'];
     
 }
